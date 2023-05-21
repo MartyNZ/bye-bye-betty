5c7c38ade7279ebccc354c130356d297d8179279
Martyn Cook <martyn@solutions-online.co.nz>
2023-05-22 11:37:57 +1200

    |

--- components/overview.vue
+++ components/overview.vue
@@ -12,11 +12,11 @@
         look good doing it.
       </p>
       <p class="pb-2">
-        The vehicle is currently being prepared for sale and will ba available
+        The vehicle is currently being prepared for sale and will be available
         shortly.
       </p>
       <p class="pb-2">
-        Complete the form below if you like to be notified when it becomes
+        Complete the form below if you would like to be notified when it becomes
         available. You can ask any question you may have through the same form.
       </p>
     </div>
